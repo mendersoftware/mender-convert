e8694c117358b8195cd5c68f6b23c8f7dc9018ad
Mirza Krak <mirza.krak@northern.tech>
2018-12-28 22:31:44 +0100

    create Mender mount-points in one single location
    There where multiple places where mount-points where created, and
    in some cases it was even duplicate code.
    
    Instead create it during convert-stage-4 where we do all generic Mender
    installation, and do it unconditionally.
    
    Changelog: None
    
    Signed-off-by: Mirza Krak <mirza.krak@northern.tech>

--- bbb-convert-stage-3.sh
+++ bbb-convert-stage-3.sh
@@ -11,10 +11,6 @@ embedded_rootfs_dir=$2
 # Copy rootfs partition.
 sudo cp -ar ${embedded_rootfs_dir}/* ${sdimg_primary_dir}
 
-# Add mountpoints.
-sudo install -d -m 755 ${sdimg_primary_dir}/boot/efi
-sudo install -d -m 755 ${sdimg_primary_dir}/data
-
 log "\tDone."
 
 exit 0
