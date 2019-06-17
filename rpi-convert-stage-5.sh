a9e0ecdf2ba9c0b71b1feed4136cf44af9f71cd5
sensslen <sensslen@users.noreply.github.com>
2019-06-17 14:36:50 +0200

    Enable U-Boot to select between compressed and uncompressed Kernels
    This Commit instructs U-Boot to be able to boot either a compressed or an uncompressed kernel. This is very useful e.g. when switching from Debian (Raspbian) created using mender-convert to a Yocto based environment that does not compress the Kernel by default.

--- rpi-convert-stage-5.sh
+++ rpi-convert-stage-5.sh
@@ -66,8 +66,16 @@ build_uboot_files() {
 	fdt addr ${fdt_addr} && fdt get value bootargs /chosen bootargs
 	run mender_setup
 	mmc dev ${mender_uboot_dev}
-	load ${mender_uboot_root} ${kernel_addr_r} /boot/zImage
-	bootz ${kernel_addr_r} - ${fdt_addr}
+	if [ -f /boot/zImage ]; then
+	  load ${mender_uboot_root} ${kernel_addr_r} /boot/zImage
+	  bootz ${kernel_addr_r} - ${fdt_addr}
+	elif [ -f /boot/uImage ]; then
+	  load ${mender_uboot_root} ${kernel_addr_r} /boot/uImage
+	  bootm ${kernel_addr_r} - ${fdt_addr}
+	else
+	  echo "No bootable Kernel found. Aborting."
+	  exit 1
+	fi
 	run mender_try_to_recover
 	EOF
 
