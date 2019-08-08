mendersoftware/mender-convert

commit d5e861b052e5f510dbe5dd78db81f49af8000245

add ServerCertificate entry in mender.conf if servert.crt was installed
MEN-2640

Changelog: Title

Based on work from:
Signed-off-by: Yevgeniy Nurseitov <enurseitov@gmail.com>

Signed-off-by: Mirza Krak <mirza.krak@northern.tech>

convert-stage-4.sh
@@ -204,6 +204,10 @@ install_files() {
   if [ -n "${server_cert}" ]; then
     sudo install -m 0444 ${server_cert} ${primary_dir}/${sysconfdir}/server.crt
   fi
+
+  if [ -e "${primary_dir}/${sysconfdir}/server.crt" ]; then
+    jq_inplace '.ServerCertificate = \"'${primary_dir}'/'${sysconfdir}'/server.crt\"' ${primary_dir}/${sysconfdir}/mender.conf
+  fi
 }
 
 do_install_mender() {

commit c10d2ade06e039b9063edb5c1105b9a43e773642

fix variable expansions for /etc/mender/servert.crt path
Currently the code expands to the following in mender.conf:

     /mender-convert/output/sdimg/primary/etc/mender/server.crt

${primary_dir} should be dropped from assignment

Fixes: d5e861b052e5 ("add ServerCertificate entry in mender.conf if servert.crt was installed")

Changelog: None

Signed-off-by: Mirza Krak <mirza.krak@northern.tech>

convert-stage-4.sh
@@ -206,7 +206,7 @@ install_files() {
   fi
 
   if [ -e "${primary_dir}/${sysconfdir}/server.crt" ]; then
-    jq_inplace '.ServerCertificate = \"'${primary_dir}'/'${sysconfdir}'/server.crt\"' ${primary_dir}/${sysconfdir}/mender.conf
+    jq_inplace '.ServerCertificate = \"/'${sysconfdir}'/server.crt\"' ${primary_dir}/${sysconfdir}/mender.conf
   fi
 }
 