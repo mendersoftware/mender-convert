mendersoftware/mender-convert

commit 9b944615a8a2530aa5e0250265938e472104d6bd

configs: Support multiple config files on command line.
The current support for this fails due to incorrect use
of bash arrays.

Also, use the printf '%q' formatting code to add proper quotes
to the config file names to ensure that it works properly
with spaces in the file names.

Signed-off-by: Drew Moseley <drew.moseley@northern.tech>

Changelog: Title

mender-convert-extract
@@ -19,6 +19,8 @@ echo "Running $(basename $0): $@"
 source modules/bootstrap.sh
 source modules/disk.sh
 
+declare -a configs
+
 disk_image=""
 while (( "$#" )); do
   case "$1" in
@@ -27,7 +29,7 @@ while (( "$#" )); do
       shift 2
       ;;
     -c | --config)
-      configs+="${2}"
+      configs+=("${2}")
       shift 2
       ;;
     -d | --disk-image)
@@ -50,7 +52,11 @@ if [ ! -e ${disk_image} ]; then
   log_fatal "File not found: ${disk_image}"
 fi
 
-source modules/config.sh "${configs[@]}"
+# Note the use of %q formatting here. This is a bash feature to add
+# proper quoting to the strings so that spaces and special characters
+# will be treated properly.  Primarily for supporting spaces in
+# pathnames and avoid splitting those into multiple parameters.
+source modules/config.sh $(printf "%q " "${configs[@]}")
 
 MKFS_VFAT="/usr/bin/mkfs.vfat"
 if [ ! -f ${MKFS_VFAT} ]; then

mender-convert-modify
@@ -39,6 +39,8 @@ source modules/bootstrap.sh
 source modules/disk.sh
 source modules/probe.sh
 
+declare -a configs
+
 while (( "$#" )); do
   case "$1" in
     -o | --overlay)
@@ -46,7 +48,7 @@ while (( "$#" )); do
       shift 2
       ;;
     -c | --config)
-      configs+="${2}"
+      configs+=("${2}")
       shift 2
       ;;
     -d | --disk-image)
@@ -59,7 +61,11 @@ while (( "$#" )); do
   esac
 done
 
-source modules/config.sh "${configs[@]}"
+# Note the use of %q formatting here. This is a bash feature to add
+# proper quoting to the strings so that spaces and special characters
+# will be treated properly.  Primarily for supporting spaces in
+# pathnames and avoid splitting those into multiple parameters.
+source modules/config.sh $(printf "%q " "${configs[@]}")
 
 boot_part=$(disk_boot_part)
 root_part=$(disk_root_part)

mender-convert-package
@@ -38,6 +38,8 @@ echo "Running $(basename $0): $@"
 source modules/bootstrap.sh
 source modules/disk.sh
 
+declare -a configs
+
 while (( "$#" )); do
   case "$1" in
     -o | --overlay)
@@ -45,7 +47,7 @@ while (( "$#" )); do
       shift 2
       ;;
     -c | --config)
-      configs+="${2}"
+      configs+=("${2}")
       shift 2
       ;;
     -d | --disk-image)
@@ -58,7 +60,11 @@ while (( "$#" )); do
   esac
 done
 
-source modules/config.sh "${configs[@]}"
+# Note the use of %q formatting here. This is a bash feature to add
+# proper quoting to the strings so that spaces and special characters
+# will be treated properly.  Primarily for supporting spaces in
+# pathnames and avoid splitting those into multiple parameters.
+source modules/config.sh $(printf "%q " "${configs[@]}")
 
 PARTED="/usr/bin/parted"
 if [ ! -f ${PARTED} ]; then

modules/config.sh
@@ -14,7 +14,10 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-for config in configs/mender_convert_config "$@"; do
-  log_info "Using configuration file: ${config}"
-  source ${config}
+# Read in the array of config files to process
+read -a configs <<< "${@}"
+configs=( "configs/mender_convert_config" "${configs[@]}" )
+for config in "${configs[@]}"; do
+    log_info "Using configuration file: ${config}"
+    source "${config}"
 done

commit 88fe7558c94c61c855f52e5b49bf74aaf9b4b8c8

add the default configuration (mender_convert_config) to the config array at creation
Current logic actually fails when no '-c/--config' options are provided, with this error:

$ MENDER_ARTIFACT_NAME="test-1" ./mender-convert --disk-image input/Armbian_5.95_Tinkerboard_Debian_buster_next_4.19.69_minimal.img
Running mender-convert-extract: --disk-image input/Armbian_5.95_Tinkerboard_Debian_buster_next_4.19.69_minimal.img
2019-10-02 12:46:39 [INFO] [mender-convert-extract] Using configuration file: configs/mender_convert_config
2019-10-02 12:46:39 [INFO] [mender-convert-extract] Using configuration file: ''
modules/config.sh: line 28: '': No such file or directory

The 'configs' array is actually not empty when passed to modules/config.sh
as it will contain "''", which will be translated as a array element and
hence the above failure.

This commit ensures that we set a valid default value to 'configs',
which is the 'mender_convert_config'. This is moved from modules/config.sh,
to the calling scripts that define the array of configs.

Fixes: 9b944615a8a ("configs: Support multiple config files on command line.")

Changelog: None

Signed-off-by: Mirza Krak <mirza.krak@northern.tech>

mender-convert-extract
@@ -19,7 +19,8 @@ echo "Running $(basename $0): $@"
 source modules/bootstrap.sh
 source modules/disk.sh
 
-declare -a configs
+# The mender_convert_config is always used and provides all the defaults
+declare -a configs=("configs/mender_convert_config")
 
 disk_image=""
 while (( "$#" )); do

mender-convert-modify
@@ -39,7 +39,8 @@ source modules/bootstrap.sh
 source modules/disk.sh
 source modules/probe.sh
 
-declare -a configs
+# The mender_convert_config is always used and provides all the defaults
+declare -a configs=("configs/mender_convert_config")
 
 while (( "$#" )); do
   case "$1" in

mender-convert-package
@@ -38,7 +38,8 @@ echo "Running $(basename $0): $@"
 source modules/bootstrap.sh
 source modules/disk.sh
 
-declare -a configs
+# The mender_convert_config is always used and provides all the defaults
+declare -a configs=("configs/mender_convert_config")
 
 while (( "$#" )); do
   case "$1" in

modules/config.sh
@@ -16,7 +16,6 @@
 
 # Read in the array of config files to process
 read -a configs <<< "${@}"
-configs=( "configs/mender_convert_config" "${configs[@]}" )
 for config in "${configs[@]}"; do
     log_info "Using configuration file: ${config}"
     source "${config}"