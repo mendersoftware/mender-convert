2e779130f269cf7468e978fd761dffbacacfd109
Ole Petter <ole.orhagen@northern.tech>
2021-02-18 11:11:01 +0100

    Update rpi matcher
    Changelog: None
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

--- scripts/monitor-rpi.js
+++ scripts/monitor-rpi.js
@@ -2,43 +2,38 @@ const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 
 const url = "http://downloads.raspberrypi.org/raspbian_lite/images"
-const imageName = "bone-debian-10.3-iot-armhf-2020-04-06-4gb.img.xz"
+const imageName = "raspbian_lite-2019-09-30"
 
-var reg = "bone-debian-(?<version>[0-9]+\.[0-9]+)-iot-armhf-(?<date>[0-9]{4}-[0-9]{2}-[0-9]{1,2})-4gb.img.xz$"
-var arr = imageName.match(reg);
+var reg = "raspbian_lite-(?<date>[0-9]{4}-[0-9]{2}-[0-9]{1,2})/(?<updated>[0-9]{4}-[0-9]{2}-[0-9]{1,2}).*$"
 
 JSDOM.fromURL(url, {}).then(dom => {
-    console.log(dom.serialize());
     var document = dom.window.document;
-    console.log(document.getElementsByTagName("list"));
-    var table = document.getElementsByTagName("list");
+    var table = document.getElementsByTagName("table");
     var rows = table[0].rows;
     var matches = [];
     for (var i=0; i< rows.length; i++) {
         var rowText = rows[i].textContent;
-        // console.log(rowText);
-        // var regMatch = rowText.match(reg);
-        // if (regMatch) {
-        //     console.log(rowText);
-        //     matches.push({
-        //         text: rowText,
-        //         version: regMatch.groups.version,
-        //         date: regMatch.groups.date,
-        //     });
-        // }
+        var regMatch = rowText.match(reg);
+        if (regMatch) {
+            matches.push(regMatch);
+        }
     }
     // Sort the accumulated matches
-    // matches.sort(function(a,b) {
-    //     let al = parseFloat(a.version);
-    //     let bl = parseFloat(b.version);
-    //     if (al == bl) {
-    //         let ad = Date.parse(a.date);
-    //         let bd = Date.parse(b.date);
-    //         return bd - ad;
-    //     }
-    //     return parseFloat(b.version) - parseFloat(a.version);
-    // });
-    // if (matches[0].text !== imageName) {
-    //     console.log("We've got a new release! \\o/");
-    // }
+    matches.sort(function(a,b) {
+        let al = Date.parse(a.groups.date);
+        let bl = Date.parse(b.groups.date);
+        if (al == bl) {
+            let ad = Date.parse(a.groups.updated);
+            let bd = Date.parse(b.groups.updated);
+            return bd - ad;
+        }
+        return bl - al;
+    });
+    var matchOn = matches[0].input.split("/")[0]
+    if (matchOn !== imageName) {
+        console.log("We've got a new release! \\o/");
+        console.log(matchOn)
+        console.log("Old match")
+        console.log(imageName)
+    }
 });
