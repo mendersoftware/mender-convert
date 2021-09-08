7981e223a8065c5db1b1a4dacb4b719e23f02f12
Kristian Amlie <kristian.amlie@northern.tech>
2021-09-08 08:49:10 +0200

    URL encode '+' characters in package names, or else we get Forbidden.
    Changelog: Fix broken download of packages when `*_VERSION` are set to
    `master`.
    
    Signed-off-by: Kristian Amlie <kristian.amlie@northern.tech>
    (cherry picked from commit ed5da386679c2995c18c522d4e2abff3614f4139)

--- modules/deb.sh
+++ modules/deb.sh
@@ -47,7 +47,8 @@ function deb_from_repo_dist_get()  {
     fi
 
     local -r filename=$(basename $deb_package_path)
-    run_and_log_cmd "wget -Nq ${repo_url}/${deb_package_path} -P ${download_dir}"
+    local -r deb_package_url=$(echo ${repo_url}/${deb_package_path} | sed 's/+/%2B/g')
+    run_and_log_cmd "wget -Nq ${deb_package_url} -P ${download_dir}"
 
     rm -f /tmp/Packages
     log_info "Successfully downloaded ${filename}"
