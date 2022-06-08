e2e70faa0e1526f757bfc62282e4f7d2eb174703
Kristian Amlie <kristian.amlie@northern.tech>
2022-06-08 10:18:52 +0200

    fix: `resource` folder needs to be inside `input` after Dockerization.
    No changelog needed, since nothing is released yet.
    
    Changelog: None
    Ticket: None
    
    Signed-off-by: Kristian Amlie <kristian.amlie@northern.tech>

--- scripts/bootstrap-rootfs-overlay-production-server.sh
+++ scripts/bootstrap-rootfs-overlay-production-server.sh
@@ -62,24 +62,24 @@ if [ -e ${output_dir} ]; then
     sudo chgrp -R $(id -g) ${output_dir}
 fi
 
-mkdir -p ${root_dir}/resources
-cat <<- EOF > ${root_dir}/resources/mender.conf
+mkdir -p ${root_dir}/input/resources
+cat <<- EOF > ${root_dir}/input/resources/mender.conf
 {
   "ServerURL": "${server_url}",
 EOF
 
 if [ -n "${server_cert}" ]; then
-    cat <<- EOF >> ${root_dir}/resources/mender.conf
+    cat <<- EOF >> ${root_dir}/input/resources/mender.conf
   "ServerCertificate": "/etc/mender/server.crt"
 EOF
     cp -f "${server_cert}" ${output_dir}/etc/mender/server.crt
 fi
 
-cat <<- EOF >> ${root_dir}/resources/mender.conf
+cat <<- EOF >> ${root_dir}/input/resources/mender.conf
 }
 EOF
 
 sudo chown -R 0 ${output_dir}
 sudo chgrp -R 0 ${output_dir}
 
-echo "Configuration file for using Production Mender Server written to: ${root_dir}/resources/mender.conf"
+echo "Configuration file for using Production Mender Server written to: ${root_dir}/input/resources/mender.conf"
